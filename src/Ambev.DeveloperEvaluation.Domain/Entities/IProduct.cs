cf841cea09d563169278bd8865c5ca2b131bd0bc
Lessandro Santana <lesantana@hotmail.com>
2025-02-11 14:14:10 -0300

    Classes das entidades

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/IProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    interface IProduct
+    {
+        /// <summary>
+        /// Obtém a nota da avaliação
+        /// </summary>
+        /// <returns>A nota da avaliação.</returns>
+        public decimal RatingRate { get; set; }
+
+        /// <summary>
+        /// Obtém a quantidade de avaliações
+        /// </summary>
+        /// <returns>A quantidade de avaliações.</returns>
+        public int RatingCount { get; set; }
+
+
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Rating.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    /// <summary>
+    /// Avaliação do produto
+    /// </summary>
+    public class Rating
+    {
+        /// <summary>
+        /// Nota da avaliação
+        /// </summary>
+        public decimal Rate { get; set; } = 0;
+
+        /// <summary>
+        /// Quantidade de avaliações
+        /// </summary>
+        public int Count { get; set; } = 0;
+    }
+}
