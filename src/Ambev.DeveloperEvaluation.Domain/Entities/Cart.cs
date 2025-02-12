d06f8fece083b4b166e45974306487cf31ac11e7
Lessandro Santana <lesantana@hotmail.com>
2025-02-12 20:03:49 -0300

    Classes das entidades

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Cart.cs
@@ -0,0 +1,57 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    /// <summary>
+    /// Classe wue representa o carrinho de compras
+    /// </summary>
+    public class Cart : BaseEntity
+    {
+        /// <summary>
+        /// Número da Venda, inteiro aleatório, não repete
+        /// </summary>
+        public int SaleNumber { get; set; } = 0;
+
+
+        //  Date when the sale was made
+        /// <summary>
+        /// Data da Venda
+        /// </summary>
+        public DateTime Date { get; set; } = DateTime.Now;
+
+
+        /// <summary>
+        /// Cliente que está comprando os produtos
+        /// </summary>
+        public Guid UserID { get; set; }
+
+        //  Total sale amount
+        /// <summary>
+        /// Valor total do carrinho de compras
+        /// </summary>
+        public decimal TotalAmount { get; set; } = 0;
+
+        /// <summary>
+        /// Nome da filial onde a venda foi efetuada
+        /// </summary>
+        public string BranchName { get; set; } = string.Empty;
+
+        //  Products
+        /// <summary>
+        /// Lista de produtos do carrinho de compras
+        /// </summary>
+        public List<CartItem> CartItemList { get; set; } = new List<CartItem>();
+
+
+
+
+
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/CartItem.cs
@@ -0,0 +1,47 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class CartItem : BaseEntity
+    {
+        /// <summary>
+        /// Identificador do produto
+        /// </summary>
+        public Guid ProductID { get; set; }
+
+        /// <summary>
+        /// Título (nome) do produto
+        /// </summary>
+        public string Title { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Preço unitário
+        /// </summary>
+        public decimal Price { get; set; } = 0;
+
+        /// <summary>
+        /// Quantidade a ser comprada do produto
+        /// </summary>
+        public int Quantity { get; set; } = 0;
+
+        /// <summary>
+        /// Disconto do produto
+        /// </summary>
+        public decimal Discount { get; set; } = 0;
+
+        /// <summary>
+        /// Valor total do item, já calculado com o desconto
+        /// </summary>
+        public decimal TotalAmount { get; set; } = 0;
+
+        /// <summary>
+        /// Item excluido/cancelado do carrinho de compras
+        /// </summary>
+        public bool ItemCanceled { get; set; } = false;
+    }
+}
