e5a2892bc905571cc946a15632a663c487b5576b
Lessandro Santana <lesantana@hotmail.com>
2025-02-12 19:28:17 -0300

    Excluir produtos

---
+++ src/Ambev.DeveloperEvaluation.Application/Products/DeleteProduct/DeleteProductCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Products.DeleteProduct
+{
+    public class DeleteProductCommand : IRequest<DeleteProductResponse>
+    {
+        /// <summary>
+        /// Identificador do produto
+        /// </summary>
+        public Guid Id { get; }
+
+        public DeleteProductCommand(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
